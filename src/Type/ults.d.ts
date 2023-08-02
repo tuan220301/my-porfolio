55fddaeefbd662938851f768d0a6fe72a85d60aa
tuan220301 <84058817+tuan220301@users.noreply.github.com>
2023-08-02 19:20:15 +0700

    config ts

---
+++ src/Type/ults.d.ts
@@ -0,0 +1,8 @@
+declare module 'troika-three-text';
+declare module '*.svg' {
+    const content: string;
+    export default content;
+}
+declare module '*.jpg';
+declare module "*.pdf";
+declare module 'react-custom-scrollbars';
