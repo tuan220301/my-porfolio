5987995098f1b1543f1992aacda7f5296e7be32e
tuan220301 <84058817+tuan220301@users.noreply.github.com>
2023-08-06 18:48:10 +0700

    test

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Layout from '../Components/Base/Layout';
 import HomeComponent from '../Components/Home';
+import { ColorModeScript, theme, ChakraProvider } from '@chakra-ui/react';
+import Scrollbars from 'react-custom-scrollbars-2';
+import NavbarComponent from '../Components/Base/Navbar';
 
 const App = () => {
     return (
-        <Layout>
-            <HomeComponent />
-        </Layout>
+        <>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+            <ChakraProvider>
+                <NavbarComponent />
+                <Scrollbars style={{ width: '100%', height: '100vh' }}>
+                    <main>
+                        <HomeComponent />
+                    </main>
+                </Scrollbars>
+            </ChakraProvider>
+        </>
     );
 };
 export default App;
